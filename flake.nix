11693aa1ac4cb35dc0f1354c4ed046e474d16848
Valentin Boettcher <hiro@protagon.space>
2021-12-09 13:19:58 +0100

    use new wrapper

--- flake.nix
+++ flake.nix
@@ -2,47 +2,15 @@
   description = "A python progress bar in ascii art.";
 
   inputs = {
+    utils.url = "github:vale981/hiro-flake-utils";
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    poetry2nix.url = "github:nix-community/poetry2nix";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    let
+  outputs = inputs@{ self, utils, nixpkgs, ... }:
+    (utils.lib.poetry2nixWrapper nixpkgs inputs {
       name = "progression";
-    in {
-      overlay = nixpkgs.lib.composeManyExtensions [
-        poetry2nix.overlay
-        (final: prev: {
-          ${name} = (prev.poetry2nix.mkPoetryApplication {
-            projectDir = ./.;
-            doCheck = false;
-            preferWheels = true;
-          });
-        })
-
-      ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlay ];
-        };
-      in
-        rec {
-          packages = {
-            ${name} = pkgs.${name};
-          };
-
-          defaultPackage = packages.${name};
-          devShell = (pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-
-            editablePackageSources = {
-              ${name} = ./${name};
-            };
-          }).env.overrideAttrs (oldAttrs: {
-            buildInputs = [ pkgs.poetry pkgs.pyright ];
-          });
-        }));
+      poetryArgs = {
+        projectDir = ./.;
+      };
+    });
 }
